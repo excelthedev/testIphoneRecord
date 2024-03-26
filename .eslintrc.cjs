54181e80ccd3c5cb41ed977f5f9374f1355d0c35
damilola <61382051+Baouldguyd@users.noreply.github.com>
2024-03-26 09:21:44 +0100

    feat(welcometext.tsx): welcome text updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "react-app",
+    // "react-app",
   ],
   overrides: [
     {
---
+++ src/pages/Homepage/Components/WelcomeSvgc/Mic.tsx
@@ -0,0 +1,113 @@
+const Mic = () => {
+  return (
+    <svg
+      width="31"
+      height="36"
+      viewBox="0 0 31 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_ddii_644_5370)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M18.2423 18.0108C17.6998 18.626 17.0088 19.0449 16.2565 19.2146C15.5042 19.3843 14.7244 19.2971 14.0158 18.9642C13.3071 18.6312 12.7014 18.0674 12.2752 17.344C11.849 16.6206 11.6214 15.7701 11.6212 14.9V9.4C11.6212 8.82218 11.7215 8.25003 11.9165 7.71619C12.1114 7.18236 12.3971 6.69731 12.7573 6.28873C13.1175 5.88015 13.5451 5.55605 14.0157 5.33493C14.4862 5.11381 14.9906 5 15.5 5C16.0094 5 16.5138 5.11381 16.9843 5.33493C17.4549 5.55605 17.8825 5.88015 18.2427 6.28873C18.6029 6.69731 18.8886 7.18236 19.0835 7.71619C19.2785 8.25003 19.3788 8.82218 19.3788 9.4V14.9C19.3786 16.0669 18.9698 17.1858 18.2423 18.0108ZM21.4307 13.3776C21.6002 13.1853 21.8162 13.0543 22.0513 13.0012C22.2865 12.9481 22.5303 12.9753 22.7518 13.0794C22.9733 13.1835 23.1626 13.3598 23.2958 13.586C23.429 13.8121 23.5 14.078 23.5 14.35V15.45C23.4975 17.6179 22.8121 19.7135 21.5674 21.3586C20.3227 23.0037 18.6008 24.09 16.7121 24.4216V25.625C16.7121 25.9897 16.5844 26.3394 16.3571 26.5973C16.1298 26.8551 15.8215 27 15.5 27C15.1785 27 14.8702 26.8551 14.6429 26.5973C14.4156 26.3394 14.2879 25.9897 14.2879 25.625V24.4216C12.3992 24.09 10.6773 23.0037 9.43258 21.3586C8.1879 19.7135 7.50249 17.6179 7.5 15.45V14.35C7.5 13.9853 7.62771 13.6356 7.85502 13.3777C8.08234 13.1199 8.39065 12.975 8.71212 12.975C9.0336 12.975 9.3419 13.1199 9.56922 13.3777C9.79654 13.6356 9.92424 13.9853 9.92424 14.35V15.45C9.92424 16.2806 10.0685 17.1031 10.3487 17.8705C10.6289 18.6379 11.0396 19.3351 11.5573 19.9225C12.0751 20.5098 12.6898 20.9757 13.3663 21.2935C14.0427 21.6114 14.7678 21.775 15.5 21.775C16.2322 21.775 16.9573 21.6114 17.6338 21.2935C18.3102 20.9757 18.9249 20.5098 19.4427 19.9225C19.9604 19.3351 20.3711 18.6379 20.6513 17.8705C20.9315 17.1031 21.0758 16.2806 21.0758 15.45V14.35C21.0758 13.9848 21.2038 13.635 21.4307 13.3776Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_ddii_644_5370"
+          x="0.748351"
+          y="0.4989"
+          width="29.5033"
+          height="35.5033"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1.12527" />
+          <feGaussianBlur stdDeviation="1.12527" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0134875 0 0 0 0 0.0641196 0 0 0 0 0.147477 0 0 0 0.26 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_644_5370"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2.25055" />
+          <feGaussianBlur stdDeviation="3.37582" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0559254 0 0 0 0 0.163133 0 0 0 0 0.339632 0 0 0 0.1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect1_dropShadow_644_5370"
+            result="effect2_dropShadow_644_5370"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect2_dropShadow_644_5370"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1.12527" />
+          <feGaussianBlur stdDeviation="0.562637" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.58 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect3_innerShadow_644_5370"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-0.562637" />
+          <feGaussianBlur stdDeviation="0.112527" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.151139 0 0 0 0 0.471907 0 0 0 0 1 0 0 0 0.13 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect3_innerShadow_644_5370"
+            result="effect4_innerShadow_644_5370"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default Mic;
