7f9cf7892d98e24828d036eb696f8bbf7e9b66ef
Abiola-Sodiq <rasababe01@gmail.com>
2024-03-26 14:44:58 +0100

    feat(dashboard): dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { setAllAppState, useGetDataQuery } from "../../store";
 import Header from "./components/Header";
-import Recorder from "./components/Recorder";
-import Welcome from "./components/Welcome";
-import WordBank from "./components/WordBank";
+
 import { endpoints } from "../../store/api/endpoints";
-import { Spin } from "antd";
+import { Progress, Spin } from "antd";
 import { userInfoObject } from "./components/Header";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import AudioChunker from "./components/RecordChunker";
+import Recording from "../../assets/icons/Recording";
+import Translate from "../../assets/icons/Translate";
+import Speak from "../../assets/icons/Speak";
+import SentenceBank from "./components/SentenceBank";
+import MicIcon from "../../assets/icons/MicIcon";
+import Button from "../../components/Button";
 
 const Dashboard = () => {
   const { data, isFetching, isLoading } = useGetDataQuery({
@@ -42,21 +45,67 @@ const Dashboard = () => {
   }, [data, dispatch, userInfo?._id]);
 
   return (
-    <div className=" min-h-[100svh] bg-[#F7F8FA]">
+    <div className=" min-h-[100svh] bg-[#F7F8FA] p-10">
       <Header
         email={userInfo?.email ?? ""}
         firstname={userInfo?.firstname ?? ""}
         lastname={userInfo?.lastname ?? ""}
         _id={userInfo?._id ?? ""}
       />
-      <Spin spinning={false}>
-        <div className="grid max-w-5xl m-auto h-[762px]">
-          <Welcome />
-          <AudioChunker />
-          <WordBank />
-          <Recorder />
+
+      <section>
+        <div className="grid grid-cols-2 mt-6 place-items-center place-content-center">
+          <div className="flex flex-wrap gap-5 h-max">
+            <span className="py-2 px-4 bg-white text-[#19213D] rounded-xl flex justify-center items-center w-max text-sm font-[gilroy-medium] gap-2">
+              <Recording />
+              <p>Record</p>
+            </span>
+            <span className="py-2 px-4 bg-white text-[#19213D] rounded-xl flex justify-center items-center w-max text-sm font-[gilroy-medium] gap-2">
+              <Translate />
+              <p>Translate</p>
+            </span>
+            <span className="py-2 px-4 bg-white text-[#19213D] rounded-xl flex justify-center items-center w-max text-sm font-[gilroy-medium] gap-2">
+              <Speak />
+              Speak
+            </span>
+          </div>
+          <div className=" text-[#333333] w-[70%] gap-3 ">
+            <span className="flex items-center justify-between text-sm">
+              <p>Total Task</p>
+              <p>50</p>
+            </span>
+            <span className="flex">
+              <p className="text-3xl ">14</p>
+              <p className="text-3xl ">/</p>
+              <p className="text-base align-text-bottom">50</p>
+            </span>
+            <Progress percent={70} showInfo={false} />
+          </div>
         </div>
-      </Spin>
+        <span className="flex items-center justify-center gap-2 mt-20 text-[#333333] text-xs md:text-base font-[gilroy-medium] ">
+          <p>Click</p>
+          <Speak />
+          <p> and read the sentence aloud in the displayed language</p>
+        </span>
+        <SentenceBank />
+        <div className="mt-10 w-[80%] sm:w-[60%] mx-auto">
+          <hr />
+          <div className="flex items-center justify-center">
+            <span className="w-16 h-16 rounded-[50%] bg-[#096A95] flex justify-center items-center ">
+              <MicIcon />
+            </span>
+          </div>
+        </div>
+        <div className="flex justify-end gap-5 mt-10">
+          <Button className=" border border-[#E3E6EA] text-[#096A95] text-base font-semi-bold font-[gilroy-semibold] !py-2 !px-4">
+            {" "}
+            Skip {">>"}{" "}
+          </Button>
+          <Button className=" border bg-[#096A9540] text-[#19213D] text-base font-semi-bold font-[gilroy-semibold] !py-2 !px-5">
+            Save
+          </Button>
+        </div>
+      </section>
     </div>
   );
 };
--- src/pages/Dashboard/components/Header.tsx
+++ src/pages/Dashboard/components/Header.tsx
@@ -47,12 +47,21 @@ const Header: React.FC<userInfoObject> = ({ email, firstname, lastname }) => {
 
   return (
     <>
-      <div className="bg-[#F7F8FA] py-4 px-2 md:px-8 flex justify-between items-center border-b-2  transition-all">
+      <div className="bg-[#FFFFFF] py-3 sm:py-6 px-2 md:px-8 flex justify-between items-center border-[0.6px] rounded-xl  transition-all w-full ">
         <LangEasyLogo />
-        <div className="flex items-center gap-2">
-          <span className="p-2 border rounded-[50%] h-10 w-10 flex items-center justify-center">
-            {firstname?.charAt(0) + " " + lastname?.charAt(0)}
+        <div className="flex flex-wrap items-center justify-center gap-2 md:gap-5">
+          <span className="flex items-center gap-2 font-[gilroy-medium]">
+            <p className="w-3 h-3 rounded-[50%] bg-black"></p>
+            <p className="text-xs font-semibold lg:text-lg"> Text Collection</p>
           </span>
+          <span className="flex items-center gap-2 font-[gilroy-medium]">
+            <p className="w-3 h-3 rounded-[50%] border border-[#666F8D]"></p>
+            <p className=" text-xs lg:text-lg font-semibold text-[#666F8D]">
+              Audio Collection
+            </p>
+          </span>
+        </div>
+        <div className="flex items-center gap-2">
           <Popover
             showArrow={false}
             content={
@@ -67,21 +76,21 @@ const Header: React.FC<userInfoObject> = ({ email, firstname, lastname }) => {
               </div>
             }
           >
-            <div
-              className="cursor-pointer "
-              // onClick={() => {
-              //   setShowLogOutBox(!showLogOutBox);
-              // }}
-            >
-              <p className="text-[#19213D] font-[gilroy-medium] text-xs md:text-base font-normal flex items-center gap-1 ">
-                <span>
-                  {firstname} {lastname}
-                </span>{" "}
-                <DropdownIcon />
-              </p>
-              <p className="text-[#666F8D] text-[10px] md:text-xs font-normal">
-                {email}
-              </p>
+            <div className="flex gap-2">
+              <span className="p-2 border rounded-[50%] h-10 w-10 flex items-center justify-center">
+                {firstname?.charAt(0) + " " + lastname?.charAt(0)}
+              </span>
+              <div className="cursor-pointer ">
+                <p className="text-[#19213D] font-[gilroy-medium] hidden md:text-base font-normal md:flex items-center gap-1 ">
+                  <span>
+                    {firstname} {lastname}
+                  </span>{" "}
+                  <DropdownIcon />
+                </p>
+                <p className="text-[#666F8D] text-[10px] md:text-xs font-normal hidden md:block">
+                  {email}
+                </p>
+              </div>
             </div>
           </Popover>
         </div>
