4391fe07e2974163fa39ec8c04d9d0fc90dffe85
damilola <61382051+Baouldguyd@users.noreply.github.com>
2024-03-27 10:41:59 +0100

    fix(eslint): update to the dev eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,28 +3,22 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    // "react-app",
-  ],
+  extends: ['eslint:recommended', 'react-app'],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "react"],
-  rules: { "react/react-in-jsx-scope": "off", "react/jsx-uses-react": "off" },
+  rules: { 'react/react-in-jsx-scope': 'off', 'react/jsx-uses-react': 'off' },
 };
